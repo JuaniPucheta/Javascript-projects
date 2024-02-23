d4da76ea8d9c6a9d02cf504d600d5ee155babce5
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2024-02-23 13:26:48 -0300

    web | rename 02

---
+++ web/src/consts.js
@@ -0,0 +1,24 @@
+export const PROJECTS = [
+  {
+    slug: "01-tinder-swipe",
+    title: "Tinder Swipe",
+    description:"Swipe de Tinder con HTML, CSS y JavaScript.",
+    learnings: [
+      "Animaciones con gestos",
+      "Eventos de mouse y touch",
+      "DOM",
+      "Transiciones CSS"
+    ],
+  },
+  {
+    slug: "02-arkanoid-game",
+    title: "Arkanoid Game",
+    description: "Juego mítico y clásico de Arkanoid para controlar con teclado",
+    learnings: [
+      "Sprites",
+      "Canvas",
+      "Eventos de teclado",
+      "RequestAnimationFrame"
+    ],
+  },
+]
